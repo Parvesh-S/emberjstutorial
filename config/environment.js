28c417aebade3b02f8429dee035ae7dd643a674d
Parvesh Samayamanthula <psamayamanthula@sokat.co>
2021-03-02 23:50:43 -0500

    added interactive and resuable components

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function (environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoicGFydmVzaHMiLCJhIjoiY2tsc3k2Nno2MDNlbTMycXg0MHlzZmp3ciJ9.kdWqJkgzXe9tz_wDdd1ePQ'
+
   return ENV;
 };
